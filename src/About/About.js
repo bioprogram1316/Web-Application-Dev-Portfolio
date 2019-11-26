97b26dbdd9d55b27aea928571e40929ff9999a40
bioprogram1316 <cabrams1316@gmail.com>
2019-11-26 17:08:59 -0500

    added about section

--- src/About/About.js
+++ src/About/About.js
@@ -16,24 +16,32 @@ const about = () => (
             <Row style={{backgroundColor: "#343a40", color: "white", border: "solid grey 1px", borderRadius: "4px"}}>
                 <Col sm={3}>
                     <Nav variant="tabs" className="flex-column">
-                        <Nav.Item>
-                            <Nav.Link eventKey="first" ><h4>My Hobbies</h4></Nav.Link>
+                    <Nav.Item>
+                            <Nav.Link eventKey="first" ><h4>About</h4></Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="second" ><h4>Contact Me</h4></Nav.Link>
+                            <Nav.Link eventKey="second" ><h4>My Hobbies</h4></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third" ><h4>Contact Me</h4></Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Col>
                 <Col sm={9}>
                     <Tab.Content style={{padding: "15px", overflow: "auto"}}>
                         <Tab.Pane eventKey="first" >
+                            <h3>I’m an New York City native. I speak both English and Turkish. I’ve lived 
+                                both in the United States and Internationally. I enjoy challenging projects and 
+                                implementing unique ideas.</h3>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second" >
                             <ul>
                                 <li><h3>Playing Tennis</h3></li>
                                 <li><h3>Horseback Riding - English Style</h3></li>
                                 <li><h3>Watching Anime</h3></li>
                             </ul>
                         </Tab.Pane>
-                        <Tab.Pane eventKey="second" >
+                        <Tab.Pane eventKey="third" >
                             <h3>Email: cdurmus@carlinedurmus.com</h3>
                         </Tab.Pane>
                     </Tab.Content>
